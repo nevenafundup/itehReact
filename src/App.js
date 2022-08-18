a65f2792e7aa95bf4c449d6e4b2523c38971d935
nevenafundup <nevenafundup7@gmail.com>
2022-08-18 17:24:24 +0200

    dodata komponenta futer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@
 import './App.css';
 import Navbar from './komponente/Navbar';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Footer from './komponente/Footer';
 function App() {
   return (
     <div  >
       <BrowserRouter className="App">
           <Navbar cartNum={0}></Navbar>
-
+          <Footer></Footer>
        </BrowserRouter>
     </div>
   );
