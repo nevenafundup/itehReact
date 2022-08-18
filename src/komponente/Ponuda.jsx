e17ff8704601e352ab7698d1ad5a55e3f91a23e6
nevenafundup <nevenafundup7@gmail.com>
2022-08-18 18:36:39 +0200

    omoguceno sortiranje proizvoda po ceni

--- src/komponente/Ponuda.jsx
+++ src/komponente/Ponuda.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Kartica from "./Kartica";
  
 
 function Ponuda({proizvodi,onAdd,onRemove}) {
+    const [sort,setSort]=useState(true);
+    function sortR(){
+        setSort(true);
+    }
+    function sortO(){
+        setSort(false);
+    }
+
   return (
-    <div className="all-products"> 
-        {proizvodi
-            .map((p)=> <Kartica key={p.id} proizvod={p} onAdd={onAdd} onRemove={onRemove}></Kartica>)
+    <div>
+        <button className="sortbtn btn" onClick={sortR}>Sortiraj rastuce</button>
+        <button className="sortbtn btn" onClick={sortO}>Sortiraj opadajuce</button>
+        <div className="all-products"> 
+        {sort==true ? 
+            <>
+            {proizvodi
+                .sort((a,b)=> a.cena<b.cena?-1:1)
+                .map((p)=> <Kartica key={p.id} proizvod={p} onAdd={onAdd} onRemove={onRemove}></Kartica>)
+            }
+            </>
+        :
+            <>
+            {proizvodi
+                .sort((a,b)=> a.cena>=b.cena?-1:1)
+                .map((p)=> <Kartica key={p.id} proizvod={p} onAdd={onAdd} onRemove={onRemove}></Kartica>)
+            }
+            </>
         }
-        
- 
+
+            
+    
+        </div>
     </div>
   );
 }
