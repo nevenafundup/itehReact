d0c60500691595058ba59adeb08308b04535c3a5
nevenafundup <nevenafundup7@gmail.com>
2022-08-19 17:47:25 +0200

    dodat css za pocetnu

--- src/komponente/Pocetna.jsx
+++ src/komponente/Pocetna.jsx
@@ -3,9 +3,15 @@ import React from "react";
 
 function Pocetna({cartNum}) {
   return (
-    <div >
+    <div className="pocetna" >
+      
         <h1>Dobrodosli u nasu knjizaru!</h1>
+        <hr />
+        <br /><br />
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quaerat, natus nemo exercitationem, reprehenderit placeat necessitatibus quibusdam, ut illum quae ipsam? Quae ratione dignissimos itaque velit aperiam? Atque, officia porro.</p>
+        <br />
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Neque eveniet dolores deserunt corporis. Quisquam amet facere dolore nobis ex itaque facilis aut voluptas earum vitae officiis ea, doloremque tenetur corrupti! Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam natus distinctio dolor. Explicabo ut, id maiores aliquid recusandae necessitatibus, quasi incidunt beatae iure at, quod minus consectetur vero ratione obcaecati.</p>
+    
     </div>
   );
 }
